f6bf77a8738e4dac69a1adada7a01e021cd73104
RobYkUuU <117177174+RobYkUuU@users.noreply.github.com>
2022-11-01 14:53:39 +0200

    Add files via upload

---
+++ AfisareNumere2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int n;
+   cin>>n;
+   for(int i=1;i<=n;i++)
+       cout<<i<<" ";
+   cout<<endl;
+   for(int i=n;i>=1;i--)
+       cout<<i<<" ";
+}
